ffb77c304a0c5d3653c28d472147a1913342f395
ktsirkou <ktsirkou@gmail.com>
2013-03-21 17:24:48 +0100

    Seconde Commit

--- js/code.js
+++ js/code.js
@@ -1,27 +1,22 @@
-function get_tweets() { 
- 
- $.ajax({  
-		
-		url: 'page.php',
-		type: 'post',
-		dataType : "json",  
-		data: '<? php echo $tweets; ?>',
- 
-        timeout:15000,  
- 
-        success : function(data)  
-        {   
- for (i=0; i<data.length; i++)  
-            {  
- $("#boxesContainer").append("<div class='box'><p>" + data[i].text)+"</p></div>";  
-            }  
-        },  
- 
-        error : function()  
-        {   
-            alert("Failure!");   
-        },  
- 
-    }); }
- 
-$(document).ready(function() { get_tweets(); });
+function displaydate(date){
+ var n = date.split("+0000");
+ var displaydate = n[0] + n[1];
+ return displaydate;
+}
+
+function appendTweets(data) {
+ for (i=data.length-1;i>=0; i--) {  
+	 $("#boxesContainer").append("<div class='box'><h2>" + displaydate(data[i].created_at) +"</h2><p>" + data[i].text+"</p></div>");
+  }  
+}
+
+function get_tweets() {
+ var url = "https://api.twitter.com/1/statuses/user_timeline.json?screen_name=CNN&count=50&include_entities=true&include_rts=true";
+ JSONP.get(url, null, function(data) {
+   appendTweets(data);
+  });
+}
+
+$(document).ready(function() { 
+  get_tweets(); 
+});
--- page.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $tweets = file_get_contents('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=tzimispanousis');
-  echo "$tweets";
- ?>
